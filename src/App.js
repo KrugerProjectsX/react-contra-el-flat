3c78e879967a17f678fefc97466f058c35397466
ivanavgonzalez <ivgonzalez@krugerschool.edu.ec>
2024-03-11 20:33:41 -0500

    style login page, create register component / page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
+import { Register } from "./pages/Register";
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path={"/"} element={<Login/>}/>
       <Route path={"/dashboard"} element={<Home/>}/>
+      <Route path={"/register"} element={<Register/>}/>
     </Routes>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,94 @@
 import { useRef } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import loginPic from "../images/loginPic.png";
+import { Error } from "../components/Error";
 
 const Login = () => {
+  const email = useRef("");
+  const password = useRef("");
+  const userRef = collection(db, "users");
+  const navigate = useNavigate();
 
-    const email = useRef("");
-    const password = useRef("");
-    const userRef = collection(db, 'users');
-    const navigate = useNavigate();
-
-    const login = async (e) => {
-        e.preventDefault();
-        const search = query(userRef, where('email', "==" , email.current.value))
-        const result = await getDocs(search);
-        if(result.docs.length > 0){
-            const user = result.docs[0].data()
-            if(user.password === password.current.value){
-                navigate("/dashboard", {replace: true})
-            }else{
-                alert("email or password incorrect")
-            }
-        }else{
-            alert("email or password incorrect")
-        }
-    };
+  const login = async (e) => {
+    e.preventDefault();
+    const search = query(userRef, where("email", "==", email.current.value));
+    const result = await getDocs(search);
+    if (result.docs.length > 0) {
+      const user = result.docs[0].data();
+      if (user.password === password.current.value) {
+        navigate("/dashboard", { replace: true });
+      } else {
+        alert("email or password incorrect");
+      }
+    } else {
+      alert("email or password incorrect");
+    }
+  };
 
   return (
-    <>
-    <div className="flex justify-center">
-      <Box
-        onSubmit={login}
-        component="form"
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          inputRef={email}
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          type="email"
-          className=""
-        />
-
-        <TextField
-          inputRef={password}
-          id="outlined-basic"
-          label="Password"
-          variant="outlined"
-          type="password"
-        />
-
-        <Button variant="contained" type="submit">Log in</Button>
-        
-      </Box>
+    <div className="flex flex-col lg:flex-row w-screen h-screen">
+      <div className="h-screen lg:w-1/2">
+        <div className="relative w-full h-full flex flex-col">
+          <img src={loginPic} alt="login" className="w-full h-full object-cover"></img>
+        </div>
+      </div>
+  
+      <div className="w-full lg:w-1/2 h-full flex flex-col bg-[#6D9773] items-center justify-center">
+        <Box
+          onSubmit={login}
+          component="form"
+          noValidate
+          autoComplete="off"
+          className="w-full lg:w-80 bg-[#FFFFFF] shadow-xl rounded-xl p-0 lg:p-6 lg:p-10"
+        >
+          <Typography variant="h4" className="m-2 text-center lg:text-left">
+            Login
+          </Typography>
+  
+          <TextField
+            inputRef={email}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            type="email"
+            className="m-2 w-sreen lg:w-full"
+          />
+  
+          <TextField
+            inputRef={password}
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+            className="m-2 w-sreen lg:w-full"
+          />
+  
+          <Button
+            variant="contained"
+            type="submit"
+            className="m-2 w-60 lg:w-full bg-[#0C3B2E]"
+          >
+            Log in
+          </Button>
+  
+          <div className="flex flex-col lg:flex-row justify-center lg:justify-start items-center lg:items-start">
+            <p className="m-2">Don't have an account?</p>
+            <Link href="/register" underline="hover" className="m-2">
+              {"Sign up"}
+            </Link>
+          </div>
+        </Box>
+      </div>
     </div>
-    </>
   );
+  
 };
 
 export { Login };
