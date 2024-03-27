2a469c0928d2a23c9ed40da9f8ab31a1b29fc259
Duard <emoya@krugerschool.edu.ec>
2024-03-27 12:34:17 -0500

    [ADD] flat routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
+import AddFlat from "./pages/AddFlat";
+import Flats from "./pages/Flats";
+import ViewFlat from "./pages/ViewFlat";
+import MyFlats from "./pages/MyFlats";
+import './App.css'
+
 
 function App() {
   return (
     <Routes>
-      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/"} element={<Login/>}/>
+      <Route path={"/dashboard"} element={<Home />}/>
+      <Route path={"/register"} element={<Register/>}/>
+      <Route path={"/profile"} element={<Profile/>}/>
+      <Route path={"/profile-edit"} element={<ProfileUpdate/>}/>
+      <Route path={"/users"} element={<Users/>}/>
+      <Route path={"/flats"} element={<Flats />}/>
+      <Route path={"/flats/new"} element={<AddFlat />}/>
+      <Route path={"/flats/view-flat"} element={<ViewFlat />}/>
+      <Route path={"/my-flats"} element={<MyFlats />}/>
     </Routes>
   );
 }
 
 export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   important: '#root',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-greenish-gray-600': '#6D9773',
+        'bg-dark-green-900': '#0C3B2E',
+        'bg-tan-600': '#BB8A52',
+        'bg-sunflower-500': '#FFBA00',
+      }
+    },
   },
   plugins: [],
   corePlugins:{
