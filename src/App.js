75447238d456bde1f03f3020b4963a858c0b4e44
ivanavgonzalez <ivgonzalez@krugerschool.edu.ec>
2024-04-02 21:09:06 -0500

    this time it should work

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ import { Profile } from "./pages/Profile";
 import { ProfileUpdate } from "./pages/ProfileUpdate";
 import "./App.css"
 import { Users } from "./pages/Users";
+import AddFlat from "./pages/AddFlat";
+import Flats from "./pages/Flats";
+import ViewFlat from "./pages/ViewFlat";
+import MyFlats from "./pages/MyFlats";
+
+import { EditFlatPage } from "./pages/EditFlatPage";
 
 function App() {
   return (
     <Routes>
+      <Route path={"/flats"} element={<Flats />}/>
+      <Route path={"/flats/new"} element={<AddFlat />}/>
+      <Route path={"/flats/view-flat"} element={<ViewFlat />}/>
+      <Route path={"/flats/view-flat/edit/:id"} element={<EditFlatPage/>}/>
+      <Route path={"/flats/my-flats"} element={<MyFlats />}/>
+      <Route path={"/flats/my-favorite-flats"} element={<MyFlats />}/>
       <Route path={"/"} element={<Login/>}/>
       <Route path={"/dashboard"} element={<Home/>}/>
       <Route path={"/register"} element={<Register/>}/>
